5a9e90aebf6bd357e0712d883c6c763c47543eb0
Deepesh Mitra <deepesh.mi26@gmail.com>
2025-11-26 08:57:11 +0530

    first commit

---
+++ first_app/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 74, 58, 102),
+        body: Center(child: Text('Hello world')),
+      ),
+    ),
+  );
+}
